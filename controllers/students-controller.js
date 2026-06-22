0f6ab73bf824030def76dae5bae63a9511e4ae59
Stanislav Gospodinov <shonito44@gmail.com>
2026-06-22 21:34:22 +0300

    update title

--- controllers/students-controller.js
+++ controllers/students-controller.js
@@ -2,7 +2,7 @@ function setup(app, students) {
   app.get('/', function(req, res) {
     let model = {
       title: "MVC Example",
-      msg: "Students Registry",
+      msg: "Hello DevOps!",
       students: students
     };
     res.render('home', model);
